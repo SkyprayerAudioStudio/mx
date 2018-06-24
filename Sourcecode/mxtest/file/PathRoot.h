4d89dae0b7dc604b90be2372db2a6b1f618250f8
Matthew James Briggs <mjb@mint.user>
2018-06-24 12:45:18 -0700

    pthreads weird

--- Sourcecode/mxtest/file/PathRoot.h
+++ Sourcecode/mxtest/file/PathRoot.h
@@ -4,10 +4,10 @@
 
 // The absolute path to the root of the repository.
 #ifndef MX_REPO_ROOT_PATH
-#define MX_REPO_ROOT_PATH "/Users/mjb/Documents/repos/mx"
+#define MX_REPO_ROOT_PATH "/home/mjb/repos/mx"
 #endif
 
 // The absolute path to the binary output directory.
 #ifndef MX_BINARY_OUTPUT_PATH
-#define MX_BINARY_OUTPUT_PATH "/Users/mjb/Documents/repos/mx/cmake-build-debug-gcc"
+#define MX_BINARY_OUTPUT_PATH "/home/mjb/repos/mx/cmake-build-debug"
 #endif
